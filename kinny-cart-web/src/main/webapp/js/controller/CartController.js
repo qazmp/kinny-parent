4cc2fae655b4227b27ddcbf3647417aff3b301a0
qgy_glb <811205785@qq.com_glb>
2019-07-05 10:53:04 +0800

    finish from cookie show and add

--- kinny-cart-web/src/main/webapp/js/controller/CartController.js
+++ kinny-cart-web/src/main/webapp/js/controller/CartController.js
@@ -2,15 +2,29 @@ app.controller('CartController', function ($scope, CartService) {
 
     $scope.cartList = [];
 
+    $scope.totalVal = {};
+
     $scope.initCartList = function () {
 
         CartService.showCarts().success(function (response) {
             if(response.success) {
                 $scope.cartList = response.data;
+                $scope.totalVal = CartService.sum($scope.cartList);
             }
         });
 
     }
+
+    $scope.addGoodsToCartList = function (itemId, num) {
+        CartService.addGoodsToCartList(itemId, num).success(function (response) {
+            if(!response.success) {
+                alert(response.message);
+            }else {
+                $scope.initCartList();
+            }
+        });
+    }
+
 
 
 });
--- kinny-cart-web/src/main/webapp/js/service/CartService.js
+++ kinny-cart-web/src/main/webapp/js/service/CartService.js
@@ -3,5 +3,24 @@ app.service('CartService', function ($http) {
    this.showCarts = function () {
        return $http.get('cart/CartList.do');
    }
+   
+   this.addGoodsToCartList = function (itemId, num) {
+       return $http.get('cart/addToCarts.do?itemId=' + itemId + '&num=' + num);
+   }
+
+
+   this.sum = function (cartList) {
+       var totalVal = {totalNum: 0, totalFee: 0};
+       for (var i = 0; i < cartList.length; i ++) {
+           var cart = cartList[i];
+           for (var j = 0; j < cart.itemList.length; j ++) {
+               var item = cart.itemList[j];
+            totalVal.totalNum += item.num;
+            totalVal.totalFee += item.totalFee;
+           }
+       }
+       return totalVal;
+   }
+
 
 });
